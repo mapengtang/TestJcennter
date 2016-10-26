b74eac05c4623cec30fd558efe648f33e9165450
mapengtang <mapengtang@enn.cn>
2016-10-26 10:18:42 +0800

    实现maven的本地电脑仓库与本地服务器的仓库上传

---
+++ localrepo/src/main/java/com/ma/localrepo/TestClass.java
@@ -0,0 +1,12 @@
+package com.ma.localrepo;
+
+/**
+ * 测试的class类
+ * Created by mapengtang on 2016/10/25.
+ */
+
+public class TestClass {
+    public static String returnStr() {
+        return "localRepoLibrary" + "返回的值为";
+    }
+}
