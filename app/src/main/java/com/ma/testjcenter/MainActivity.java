436c9413a084b1839d166bddf74ab2c3f1868094
mapengtang <mapengtang@enn.cn>
2016-10-26 11:05:11 +0800

    修复问题

--- app/src/main/java/com/ma/testjcenter/MainActivity.java
+++ app/src/main/java/com/ma/testjcenter/MainActivity.java
@@ -2,6 +2,9 @@ package com.ma.testjcenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ma.localrepo.TestClass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        //本地没有仓库的内容则会报错
+        TextView tv = (TextView) findViewById(R.id.show_msg);
+        tv.setText(TestClass.returnStr());
     }
 }
